2aea8c2ddf8b6b3963b3374736f02348b64eda58
mukai03 <mukai5186@gmail.com>
2022-03-11 19:57:41 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        // && "и"  || "или"
+    }
+
+    public static void test(String[] args) {
+        // "test" - это пример. "public static void" - это метод
+    }
+
+    public static void make(String[] args) {
+        //
+    }
+
+}
